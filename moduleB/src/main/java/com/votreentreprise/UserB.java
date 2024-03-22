1f42e848d5a6f2da5c249d3644a250ec78e9176d
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2024-03-22 23:15:42 +0100

    Batman! (this commit has no parents)

---
+++ moduleB/src/main/java/com/votreentreprise/UserB.java
@@ -0,0 +1,5 @@
+package com.votreentreprise;
+
+public class UserB {
+    
+}
--- moduleC/src/main/java/com/votreentreprise/App.java
+++ moduleC/src/main/java/com/votreentreprise/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
+        UserA totoA = new UserA();
+        UserB totoB = new UserB();
         System.out.println( "Hello World!" );
     }
 }
